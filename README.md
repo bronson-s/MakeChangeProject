353bec86b1af3393807c0ae3b07b46317a5f9f79
Bronson Spies <spiesbronson@gmail.com>
2020-08-29 12:55:33 -0600

    Final Draft. Added method in the else statement for improved readability.

--- README.md
+++ README.md
@@ -1,13 +1,24 @@
-## Make Change Project
-
+##First Skill Distillery HW:
+###Make Change Project
 
 ### Overview
-summary goes here
+For this project we were asked to create a program that acts like a cash register
+We were asked to take user input regarding the cost of the item and the amount
+provided by the customer. The program is then required to display an appropriate
+message if the amount was exact or to little. The amount provided by the customer
+exceeds the unit price then the program should calculate and display the change
+owed
 
 ### Topics
 * Taking User Input
-* casting
+* Casting
+* Methods
+* Loops
+* Conditionals
 
 #### How to Run
-1.
-2. 
+1. Hit run with in eclipse
+2. Follow the prompt and enter a number
+3. Follow the next prompt and enter another number
+4. If the numbers you entered reach the makeChange method then you will be view youre
+change
--- src/makechange/makeChange.java
+++ src/makechange/makeChange.java
@@ -5,14 +5,6 @@ import java.util.Scanner;
 public class makeChange {
 	public static void main(String[] args) {
 		Scanner kb = new Scanner(System.in);
-		
-		int tenDB = 0;
-		int fiveDB = 0;
-		int oneDB = 0;
-		double qaurter = 0;
-		double dime = 0;
-		double nick = 0;
-		double penn = 0;
 
 		System.out.println("please enter item price: ");
 		double price = kb.nextDouble();
@@ -30,51 +22,66 @@ public class makeChange {
 		}
 
 		else {
-			double change = amount - price;
-			System.out.println("Change = " + change);
-
-			while (change >= 10) {
-				tenDB++;
-				change -= 10;
-			}
-			while (change >= 5) {
-				fiveDB++;
-				change -= 5;
-			}
-			while (change >= 1) {
-				oneDB++;
-				change -= 1;
-			}
-			while (change >= .25) {
-				qaurter++;
-				change -= .25;
-			}
-			while (change >= .1) {
-				dime++;
-				change -= .1;
-			}
-			while (change >= .05) {
-				nick++;
-				change -= .05;
-			}
-			while (change >= .01) {
-				penn++;
-				change -= .01;
-			}
-			System.out.print(tenDB + " ten dollar bills");
-			System.out.print(" " + fiveDB + " five dollar bills");
-			System.out.println(" " + oneDB +" one dollar bills");
-			System.out.println(" and ");
-			System.out.print(qaurter + " qaurters");
-			System.out.print(dime + " dimes");
-			System.out.print(nick +" nickles");
-			System.out.print("and " + penn +" pennies");
-			
-			
-			
-			
+			makeChange(amount, price);
 
 		}
+
+	}
+
+	public static void makeChange(double n, double x) {
+		int tenDB = 0;
+		int fiveDB = 0;
+		int oneDB = 0;
+		int qaurter = 0;
+		int dime = 0;
+		int nick = 0;
+		int penn = 0;
+
+		double change = n - x;
+		System.out.println();
+		System.out.println("Change = " + change);
+		System.out.println();
+		System.out.println("Your change is: ");
+		System.out.println();
+
+		while (change >= 10) {
+			tenDB++;
+			change -= 10;
+		}
+		while (change >= 5) {
+			fiveDB++;
+			change -= 5;
+		}
+		while (change >= 1) {
+			oneDB++;
+			change -= 1;
+		}
+		while (change >= .25) {
+			qaurter++;
+			change -= .25;
+		}
+		while (change >= .1) {
+			dime++;
+			change -= .1;
+		}
+		while (change >= .05) {
+			nick++;
+			change -= .05;
+		}
+		while (change >= .01) {
+			penn++;
+			change -= .01;
+		}
+		System.out.print(tenDB + " Tens");
+		System.out.print(" " + fiveDB + " Fives");
+		System.out.println(" " + oneDB + " Ones");
+		System.out.println();
+		System.out.println("AND ");
+		System.out.println();
+		System.out.print(qaurter + " qaurters");
+		System.out.print(" " + dime + " dimes");
+		System.out.print(" " + nick + " nickles");
+		System.out.print(" and " + penn + " pennies");
 
 	}
 }
